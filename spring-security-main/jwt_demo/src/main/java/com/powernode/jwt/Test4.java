2394541072a7257c9843c4b869a887dde8e1cfb2
sjf <sunjianfei1029@126.com>
2022-09-29 14:46:20 +0800

    修改Test4.java代码

--- spring-security-main/jwt_demo/src/main/java/com/powernode/jwt/Test4.java
+++ spring-security-main/jwt_demo/src/main/java/com/powernode/jwt/Test4.java
@@ -1,7 +1,10 @@
 // 这是Test3类,是张三写的，用来测试的
 public class Lisi{
 	public static void mian(String[] args) {
-		System.out.printlnt("Hello");
+		for(int i = 0; i < 10; i++) {
+			System.out.printlnt("Hello");
+		}
+		
 	}
 
 }
